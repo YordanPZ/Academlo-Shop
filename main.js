c8ccdb3d99d63b7f032c9f5a7ea4b86c5f0b374a
YordanPZ <yordanpz@hotmail.com>
2023-06-11 14:47:18 -0300

    Estilos aplicados

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
+//! Variables
 const bbdd = "https://ecommercebackend.fundamentos-29.repl.co/"
-
-let datos
-
+let cartList= document.querySelector(".section2__cart-list")
 const articles = document.querySelector("#articles")
+const navCart = document.querySelector(".nav__cart")
+let cartVisible = document.querySelector(".main__section2")
 
-function getItems(){
-    axios.get(bbdd)
-        .then((response)=>{
-            const items = response.data
-            printItems(items)
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
+//! Hacer la peticion a la API
+function getItems() {
+  axios
+    .get(bbdd)
+    .then((response) => {
+      const items = response.data;
+      printItems(items);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
- getItems()
-
-function printItems(items){
-    let hmtl = ""
-    for (const item of items) {
-        hmtl += `
+getItems();
+//! Imprimir los elementos en la grilla
+function printItems(items) {
+  let hmtl = "";
+  for (const item of items) {
+    hmtl += `
         <div class="section1__article">
             <div class="section1__article-body">
                 <div>
@@ -37,35 +40,46 @@ function printItems(items){
             </div>
       </div>
       `;
-    }
-    articles.innerHTML = hmtl
+  }
+  articles.innerHTML = hmtl;
 }
-
-let cartList= document.querySelector(".section2__cart-list")
-    document.querySelector("#articles").addEventListener("click", (event)=>{
-        if(event.target.classList.contains("section1__article-button")) {
-            let hola = event.target.attributes.class.textContent.split(" ")[1]
-            console.log(hola);
-            function itemCart(){
-                axios.get(bbdd)
-                .then((response)=>{
-                    const items = response.data
-                    let cart = ""
-                    for (let item of items) {
-                        if(item.id === Number(hola)){
-                            cart+= `
-                            <li>Nombre: ${item.name}</li>
-                            <li>precio: ${item.price}</li>`
-                        }
-                    }
-                    cartList.innerHTML +=cart
-                })
-                .catch((error)=>{
-                    console.log(error)
-                })   
+//! Agregar elementos al Carrito
+document.querySelector("#articles").addEventListener("click", (event) => {
+  if (event.target.classList.contains("section1__article-button")) {
+    let id = event.target.attributes.class.textContent.split(" ")[1];
+    function itemCart() {
+      axios
+        .get(bbdd)
+        .then((response) => {
+          const items = response.data;
+          let cart = "";
+          let total = 0
+          for (let item of items) {
+            if (item.id === Number(id)) {
+              cart += `
+                    <li>${item.name}</li>
+                    <img src="${item.image}" alt="#" class="section2__cart-img">
+                    <li>$ ${item.price}</li>
+                    <hr>
+                    `;
+                    total+=item.price
             }
-            itemCart()
-        }
+          }
+
+          cartList.innerHTML += cart;
+          cartList.innerHTML
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    itemCart();
+  }
+});
+//! Aparecer y esconder el Carrito
+navCart.addEventListener("click", ()=>{
+    cartVisible.classList.toggle("main__section2--toggle2")
+    document.querySelector(".main__section1").classList.toggle("blurmain")
+
+
 })
-
-
